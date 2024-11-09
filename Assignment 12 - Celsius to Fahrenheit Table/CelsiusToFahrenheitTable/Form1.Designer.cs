2639a30f07c1f466771de2dace887f06429f6039
Adam Malec <167900556+themadadder@users.noreply.github.com>
2024-11-09 11:02:12 -0500

    Add files via upload

---
+++ Assignment 12 - Celsius to Fahrenheit Table/CelsiusToFahrenheitTable/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CelsiusToFahrenheitTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.temperatureListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // temperatureListBox
+            // 
+            this.temperatureListBox.FormattingEnabled = true;
+            this.temperatureListBox.Location = new System.Drawing.Point(12, 12);
+            this.temperatureListBox.Name = "temperatureListBox";
+            this.temperatureListBox.Size = new System.Drawing.Size(282, 329);
+            this.temperatureListBox.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(117, 352);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 387);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.temperatureListBox);
+            this.Name = "Form1";
+            this.Text = "Celsius to Fahrenheit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox temperatureListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Assignment 12 - Celsius to Fahrenheit Table/CelsiusToFahrenheitTable/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusToFahrenheitTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initializing variables
+            double fahrenheit;
+            double celsius;
+
+            // Adding a header to the table
+            temperatureListBox.Items.Add("Celsius\t\tFahrenheit");
+
+            // Display the values for celsius to fahrenheit conversion with a for loop
+            for (celsius = 0; celsius <= 100; celsius++) // I wanted to start at 0 and go all the way up to boiling point
+            {
+                fahrenheit = (9.0 / 5.0) * celsius + 32; // using the provided formula to calculate fahrenheit temps from celsius
+                temperatureListBox.Items.Add(celsius + "\t|\t" + fahrenheit); // iterating new lines to the list box for each celsius value
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
